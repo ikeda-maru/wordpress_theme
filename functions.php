df32dc60dbf436eae035fa9056eac5368c51f91f
atsushi-maru <ikeda.atsushi00@gmail.com>
2022-10-30 08:13:14 +0900

    create single.php

--- functions.php
+++ functions.php
@@ -3,3 +3,11 @@
 add_action('wp_body_open', function() {
   echo '<!--wp_body_open action hook-->';
 });
+
+add_action('init', function() {
+  add_theme_support('post-thumbnails');
+});
+
+add_action('after_setup_theme', function() {
+  add_theme_support( 'title-tag' );
+});

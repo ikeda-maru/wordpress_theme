407960f97fd37d605364282b0f5ad4e1da59d726
atsushi-maru <ikeda.atsushi00@gmail.com>
2022-10-30 09:33:29 +0900

    build single.php, etc.

--- includes/header-single.php
+++ includes/header-single.php
@@ -3,7 +3,12 @@
     <div class="row gx-4 gx-lg-5 justify-content-center">
       <div class="col-md-10 col-lg-8 col-xl-7">
         <div class="site-heading">
-          <h1><?php bloginfo('name'); ?></h1>
+          <h1><?php the_title(); ?>/h1>
+          <p class="subheading">
+            Posted by
+            <a href="#!"><?php the+author(); ?></a>
+            on <?php the_time('Y年n月j日 g:i a'); ?>
+          </p>
           <span class="subheading"><?php bloginfo('description'); ?></span>
         </div>
       </div>
